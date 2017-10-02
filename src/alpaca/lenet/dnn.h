9a131d807c7f997658a2b7213af9ea3976d97057
g-ram <ggobieski@yahoo.com>
2017-10-02 16:06:05 -0400

    removing lenet from tracking

--- src/alpaca/lenet/dnn.h
+++
@@ -1,6 +0,0 @@
-#ifndef DNN_H
-#define DNN_H
-
-#define SWAP_BUF(a, b) { mat *c = a; a = b; b = c; }
-
-#endif
--- src/alpaca/lenet/mat.d
+++
@@ -1,6 +0,0 @@
-/media/sf_debian/alpaca/src/alpaca/lenet/mat.bc: \
-  /media/sf_debian/alpaca/src/alpaca/lenet/mat.c \
-  /media/sf_debian/alpaca/src/alpaca/lenet/blas.h \
-  /media/sf_debian/alpaca/src/alpaca/lenet/types.h \
-  /media/sf_debian/alpaca/src/alpaca/lenet/fixed.h \
-  /media/sf_debian/alpaca/src/alpaca/lenet/mat.h
